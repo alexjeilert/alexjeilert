fd3146505af6d720cd6ce00a8056a721b204665b
Alexander Eilert <57025593+alexjeilert@users.noreply.github.com>
2022-08-02 16:06:52 -0500

    Created Project

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header class="bg-white">
+    
+<div class="bg-white text-gray-600"> fff
+
+
+</div>
+    
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
